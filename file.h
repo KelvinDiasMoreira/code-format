13144f74199eac21934743e2adb2b87bcfa4711c
Kelvin <kelvingremista2015@hotmail.com>
2026-01-18 22:04:21 -0300

    remove function deleted from file.c in file.h

--- file.h
+++ file.h
@@ -9,6 +9,5 @@ typedef struct FILE_HANDLER {
 
 file_handler_t *get_file_handler(char *path);
 int allocate_buffer(file_handler_t *file);
-void init_buffer(file_handler_t *file);
 int get_file_size(file_handler_t *file);
 void close_file_handler(file_handler_t *file);
