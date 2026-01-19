1d84d80a26ee595f444910965a3363d641564580
Kelvin <kelvingremista2015@hotmail.com>
2026-01-18 22:16:21 -0300

    feat: work on change buffer to added formattation

--- main.c
+++ main.c
@@ -3,8 +3,22 @@
 
 int main()
 {
-    file_handler_t *file = get_file_handler("program.c");    
-    printf("%s", file->buffer);
+    file_handler_t *file = get_file_handler("program.c");
+    printf("--------------------------------------------------\n");
+    printf("%s\n", file->buffer);
+    printf("--------------------------------------------------\n");
+    int fn_maybe = 0;
+    for(long i = 0; i < file->file_size; i++){
+        // printf("%c\n", file->buffer[i]);
+        char curr_char = file->buffer[i];
+        if(curr_char == '('){
+            fn_maybe = 1;
+            printf("'(' in i: %ld\n", i);
+        } else if (curr_char == ')' && fn_maybe){
+            printf("')' in i: %ld\n", i);
+        }
+    }
+    printf("%d", fn_maybe);
     close_file_handler(file);
     free(file->buffer);
     free(file);
