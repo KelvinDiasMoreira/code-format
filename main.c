16a5f4d98488a6c14c35d8bf8cc145d5bc393102
Kelvin <kelvingremista2015@hotmail.com>
2026-01-20 21:48:36 -0300

    initial formatting

--- main.c
+++ main.c
@@ -1,26 +1,61 @@
 #include "file.h"
+#include "alloc.h"
 // \ //
+
+/*
+    run through spaces 
+*/
+void rts(char **buffer)
+{
+    while(**buffer == ' '){
+        (*buffer)++;
+    }
+}
 
 int main()
 {
     file_handler_t *file = get_file_handler("program.c");
-    printf("--------------------------------------------------\n");
-    printf("%s\n", file->buffer);
-    printf("--------------------------------------------------\n");
+    char *new_buff = alloc_string(file->file_size);
+    char *save_ptr_buffer = file->buffer;
     int fn_maybe = 0;
-    for(long i = 0; i < file->file_size; i++){
-        // printf("%c\n", file->buffer[i]);
-        char curr_char = file->buffer[i];
-        if(curr_char == '('){
-            fn_maybe = 1;
-            printf("'(' in i: %ld\n", i);
-        } else if (curr_char == ')' && fn_maybe){
-            printf("')' in i: %ld\n", i);
+    int count = 0;
+    printf("------------------OLD BUFF--------------------------------\n");
+    printf("%s\n", file->buffer);
+    printf("------------------OLD BUFF--------------------------------\n");
+    while (*file->buffer != '\0'){
+        char curr_char = *file->buffer;
+        switch (curr_char) {
+            case '(':
+                fn_maybe = 1;
+                new_buff[count] = curr_char;
+                file->buffer++;
+                count++;
+                printf("'(' in i: %d\n", count);
+                break;
+            case ')':
+                new_buff[count] = curr_char;
+                file->buffer++;
+                count++;
+                if(fn_maybe){
+                    printf("')' in i: %d\n", count);
+                    rts(&file->buffer);
+                    if(*file->buffer == '{'){
+                        printf("TODO: NEED REMOVE, NEW LINE AND ADD\n");
+                    }
+                }
+                break;
+            default:
+                new_buff[count] = curr_char;
+                file->buffer++;
+                count++;
         }
     }
-    printf("%d", fn_maybe);
+    printf("------------------NEW BUFF--------------------------------\n");
+    printf("%s\n", new_buff);
+    printf("------------------NEW BUFF--------------------------------\n");
     close_file_handler(file);
-    free(file->buffer);
+    free(save_ptr_buffer);
     free(file);
+    free(new_buff);
     return 0;
 }
