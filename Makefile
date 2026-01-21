ef5f15072c8f71557e3fe19aa66985956bc84159
Kelvin <kelvingremista2015@hotmail.com>
2026-01-21 15:23:19 -0300

    logic of formatting and update makefile to the new format helper

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CC = gcc
 TARGET = build/c-format
-SOURCES = main.c file.c alloc.c
+SOURCES = main.c file.c alloc.c format.c
 
 default: 
 	cls
--- main.c
+++ main.c
@@ -1,59 +1,91 @@
 #include "file.h"
 #include "alloc.h"
+#include "format.h"
 // \ //
-
-/*
-    run through spaces 
-*/
-void rts(char **buffer)
-{
-    while(**buffer == ' '){
-        (*buffer)++;
-    }
-}
 
 int main()
 {
     file_handler_t *file = get_file_handler("program.c");
-    char *new_buff = alloc_string(file->file_size);
+    char *new_buff = alloc_string(file->file_size * 2);
     char *save_ptr_buffer = file->buffer;
-    int fn_maybe = 0;
-    int count = 0;
+    /*
+        Format
+    */
+    fmt_t f = {0};
     printf("------------------OLD BUFF--------------------------------\n");
     printf("%s\n", file->buffer);
     printf("------------------OLD BUFF--------------------------------\n");
     while (*file->buffer != '\0'){
         char curr_char = *file->buffer;
         switch (curr_char) {
-            case '(':
-                fn_maybe = 1;
-                new_buff[count] = curr_char;
+            case ' ':
+                new_buff[f.bp] = curr_char;
                 file->buffer++;
-                count++;
-                printf("'(' in i: %d\n", count);
+                f.bp++;
+                f.lsp++;
+                break;
+            case '\n':
+                new_buff[f.bp] = curr_char;
+                file->buffer++;
+                f.bp++;
+                f.lsp = 0;
+                f.l++;
+                break;
+            case '(':
+                f.fn = 1;
+                new_buff[f.bp] = curr_char;
+                file->buffer++;
+                f.bp++;
                 break;
             case ')':
-                new_buff[count] = curr_char;
+                new_buff[f.bp] = curr_char;
                 file->buffer++;
-                count++;
-                if(fn_maybe){
-                    printf("')' in i: %d\n", count);
+                f.bp++;
+                if(f.fn == 1){
                     rts(&file->buffer);
                     if(*file->buffer == '{'){
-                        printf("TODO: NEED REMOVE, NEW LINE AND ADD\n");
+                        printf("line %d, pos %d: (NEW LINE, ADD line space)\n", f.l, f.bp);
+                        new_buff[f.bp] = '\n';
+                        f.l++;
+                        f.bp++;
+                        if(f.lsp > 1){
+                            /*
+                                TODO: refactor to use "\t" cause imagine that we have 8 of line space
+                                if we use "' '" wee need 8 byte, but if we use "\t" its just 1 byte.
+                                make sense ?
+                            */
+                            // for(int i = 0 ; i < f.lsp ; i++){
+                                // new_buff[f.bp] = ' ';
+                                // f.bp++;
+                            // }
+                            if(f.lsp == 4){
+                                new_buff[f.bp] = '\t';
+                                f.bp++;
+                            } else {
+                                int calc = f.lsp & 4;
+                                if(calc == 0){
+                                    for(int i=0;i<(f.lsp/4);i++){
+                                        new_buff[f.bp] = '\t';
+                                        f.bp++;
+                                    }
+                                }
+                                printf("TODO: IDENTATION, LINE SPACE: %d, %d\n", f.lsp, calc);
+                            }
+                        }
+                        f.fn = 0;
                     }
                 }
                 break;
             default:
-                new_buff[count] = curr_char;
+                new_buff[f.bp] = curr_char;
                 file->buffer++;
-                count++;
+                f.bp++;
         }
     }
+    write_file(OUTPUT, new_buff);
     printf("------------------NEW BUFF--------------------------------\n");
     printf("%s\n", new_buff);
     printf("------------------NEW BUFF--------------------------------\n");
-    close_file_handler(file);
     free(save_ptr_buffer);
     free(file);
     free(new_buff);
