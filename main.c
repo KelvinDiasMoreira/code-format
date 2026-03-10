1909b140092ec7ba231997dfe6ab378810b270bb
Kelvin <kelvingremista2015@hotmail.com>
2026-03-09 23:53:33 -0300

    feat: identation

--- main.c
+++ main.c
@@ -57,18 +57,31 @@ int main()
                             }
                         }
                     }
-                    /* We need remove spaces */ 
+                    /* 
+                        We need remove spaces 
+                        TODO: fix hex string "D", causing a "\n"
+                    */ 
                     else if(space_in_line > ctx.tlvl * IDS)
                     {
                         printf("LINE %d NEED REMOVE SOME SPACE, CURRENT: %d, NEED: %d\n", ctx.l, space_in_line, ctx.tlvl * IDS);
-                        printf("CHAR: %c\n", buffer[ctx.bp]);
-                        // file->buffer--;
-                        for(int i = space_in_line; i > ctx.tlvl * IDS; i--){
-                            // printf("AAA: %d\n", i);
-                            // file->buffer--;
+                        if(*file->buffer == '}'){
+                        } else {
+                            int space = 0;
+                            while (space < ctx.tlvl * IDS){
+                                buffer[ctx.bp] = ' ';
+                                ctx.bp++;
+                                space++;
+                            }
+                            while (*file->buffer != '\n' && *file->buffer != '\0'){
+                                buffer[ctx.bp] = *file->buffer;
+                                ctx.bp++;
+                                file->buffer++;
+                            }
+                            continue;
                         }
                     }
                     /* get back to initial buffer position */
+                    /* TODO: REMOVE THIS */
                     while (walk_count > 0) {
                         walk_count--;
                         file->buffer--;
