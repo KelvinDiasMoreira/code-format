b4420932b7d1f15f3aaf9f50cda6076f667be409
Kelvin <kelvindiasmoreira@hotmail.com>
2026-03-24 21:46:13 -0300

    now can identify the level of switch cases

--- main.c
+++ main.c
@@ -3,9 +3,35 @@
 #include "alloc.h"
 #include "format.h"
 
+/* TODO: change this definitions to other file */
+#define BREAK_STR "break"
+#define SWITCH_STR "switch"
+#define SWITCH_SIZE 6
+#define BREAK_SIZE 5
+
+
+void is_break(char *b, fmt_t *ctx)
+{   
+    char *tstr = alloc_string(sizeof(char) * BREAK_SIZE);
+    int tstrp = 0;
+    int curr_pos = ctx->bp - 1;
+    /* 
+        <= cause the curr_pos is the 'k' 
+        and (curr_pos + 1) cause we get empty space
+    */
+    for(int i = ((curr_pos + 1 ) - BREAK_SIZE); i <= curr_pos; i++){
+        tstr[tstrp] = b[i];
+        tstrp++;
+    }
+    if(strcmp(tstr, BREAK_STR) == 0){
+        ctx->tlvl--;
+    }
+    free(tstr);
+}
+
 void is_switch(char *b, fmt_t *ctx)
 {
-    /* shit code ahead  */
+    /* shit code ahead, (PLEASE REFACTOR LATER)  */
     int ipos = ctx->bp - 1;
     /* if "switch ()" dont will work....
        need fix, but not now
@@ -14,15 +40,15 @@ void is_switch(char *b, fmt_t *ctx)
         ipos--;
     }
     /* maybe is a "switch" -> 6 */
-    if(((ctx->bp - 1) - (ipos + 1)) == 6){
-        char *tstr = alloc_string(sizeof(char) * 6);
+    if(((ctx->bp - 1) - (ipos + 1)) == SWITCH_SIZE){
+        char *tstr = alloc_string(sizeof(char) * SWITCH_SIZE);
         int tstrc = 0;
         for(int i = ipos + 1; i < ctx->bp - 1; i++){
             tstr[tstrc] = b[i];
             tstrc++;
         }
         tstr[tstrc + 1] = '\0';
-        if(strcmp(tstr, "switch") == 0){
+        if(strcmp(tstr, SWITCH_STR) == 0){
             ctx->swt = 1;
         }
         free(tstr);
@@ -50,20 +76,36 @@ int main()
     while (*file->buffer != '\0'){
         char cc = *file->buffer;
         switch (cc) {
+            case 'k':
+                mbfb(cc, file, &ctx, buffer);
+                if(ctx.swt){
+                    is_break(buffer, &ctx);
+                }
+                break;
+            case ':':
+                if(ctx.swt){  
+                    ctx.tlvl++;
+                }
+                mbfb(cc, file, &ctx, buffer);
+                break;
             case '{':
                 ctx.tlvl++;
                 mbfb(cc, file, &ctx, buffer);
                 break;
             case '}':
                 ctx.tlvl--;
+                if(ctx.swt){
+                    ctx.swt = 0;
+                }
                 mbfb(cc, file, &ctx, buffer);
+                
                 break;
             case '\n':
                 mbfb(cc, file, &ctx, buffer);
                 ctx.l++;
                 if(ctx.tlvl > 0 && *file->buffer != '}'){ 
                     rts(&file->buffer);
-                    /* If we found "}" means that we need subtract 1 lvl of the three level to better identation */
+                    /* If we found "}" means that we need subtract 1 lvl of the three to better identation */
                     if(*file->buffer == '}'){
                         for(int i=0;i<(ctx.tlvl - 1) * IDS; i++){
                             buffer[ctx.bp] = ' ';
